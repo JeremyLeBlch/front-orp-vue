3b615968786003854feb60bdb584ef0ea290a439
julienEspenanBaron <127543591+julienEspenanBaron@users.noreply.github.com>
2023-10-01 18:56:47 +0200

    flex to navbar + connect logout action

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-content-between flex-wrap">
     <div class="card flex align-items-center justify-content-center">
-      <Sidebar v-model:visible="visible" >
-        <img class="flex justify-content-start" src="../assets/logo.svg" alt="Orp logo" width="100">
+      <Sidebar class="flex justify-content-between flex-wrap" v-model:visible="visible" >
+        <img class="flex align-items-center justify-content-center" src="../assets/logo.svg" alt="Orp logo" width="100">
 
         <h2>Sidebar</h2>
         <p>hello sidebard.</p>
@@ -10,7 +10,7 @@
       <Button icon="pi pi-bars" @click="visible = true" />
     </div>
     <div class="card flex align-items-center justify-content-center">
-      <Button type="button" label="Toggle" @click="toggle" aria-haspopup="true" aria-controls="overlay_tmenu" />
+      <Button type="button" icon="pi pi-user" @click="toggleMenu" aria-haspopup="true" aria-controls="overlay_tmenu" />
       <TieredMenu ref="menu" id="overlay_tmenu" :model="items" popup />
     </div>
   </div>
@@ -18,56 +18,35 @@
 
 
 <script setup>
+  import {useAuthStore} from "@/stores/auth-store";
+  const authStore = useAuthStore();
   import Sidebar from 'primevue/sidebar';
   const visible = ref(false);
   import { ref } from "vue";
   import TieredMenu from 'primevue/tieredmenu';
+  import 'primeicons/primeicons.css';
   const menu = ref();
   const items = ref([
     {
       label: 'Users',
       icon: 'pi pi-fw pi-user',
       items: [
-        {
-          label: 'New',
-          icon: 'pi pi-fw pi-user-plus'
-        },
-        {
-          label: 'Delete',
-          icon: 'pi pi-fw pi-user-minus'
-        },
-        {
-          label: 'Search',
-          icon: 'pi pi-fw pi-users',
-          items: [
-            {
-              label: 'Filter',
-              icon: 'pi pi-fw pi-filter',
-              items: [
-                {
-                  label: 'Print',
-                  icon: 'pi pi-fw pi-print'
-                }
-              ]
-            },
-            {
-              icon: 'pi pi-fw pi-bars',
-              label: 'List'
-            }
-          ]
-        }
+
       ]
     },
     {
       separator: true
     },
     {
-      label: 'Logout' ,
-      icon: 'pi pi-fw pi-power-off'
+      label: 'Logout',
+      icon: 'pi pi-fw pi-power-off',
+      command: () => {
+        authStore.logout();
+      }
     }
   ]);
 
-  const toggle = (event) => {
+  const toggleMenu = (event) => {
     menu.value.toggle(event);
   };
 </script>
